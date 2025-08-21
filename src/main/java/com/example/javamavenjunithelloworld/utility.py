431138e1bee49e4a9933cd05307240126693c068
AdithyaSirigineedi <adithya.collector@gmail.com>
2025-08-21 11:35:11 +0530

    Added Utility file (python)

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.py
@@ -0,0 +1,11 @@
+def checkEvenOdd(n):
+    if n % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+n=int(input("Enter a number: "))
+checkEvenOdd(n)
+
+def Adithya():
+    print("Hello, I am Adithya")
+Adithya()
